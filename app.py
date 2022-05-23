442241fa500641d2eafd8fe74cd4f0e4ccb0aa2e
Felipeamadori <49175866+Felipeamadori@users.noreply.github.com>
2022-05-22 22:05:49 -0300

    models

--- app.py
+++ app.py
@@ -1,9 +1,59 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/campos', methods=['GET'])
+def get_campos():
+    campos = Campo.query.all()
+    return jsonify([campo.to_json() for campo in campos])
+
+@app.route("/campo/<int:id>", methods=["GET"])
+def get_campo(id):
+    campo = Campo.query.get(id)
+    if campo is None:
+        abort(404)
+    return jsonify(campo.to_json())
+    
+@app.route("/campo/<int:id>", methods=["DELETE"])
+def delete_campo(id):
+    campo = Campo.query.get(id)
+    if campo is None:
+        abort(404)
+    db.session.delete(campo)
+    db.session.commit()
+    return jsonify({'result': True})
+
+@app.route('/campo', methods=['POST'])
+def create_campo():
+    if not request.json:
+        abort(400)
+    campo = campo(
+        nome=request.json.get('nome'),
+        tipo=request.json.get('tipo'),
+        condicao=request.json.get('condicao')
+    )
+    db.session.add(campo)
+    db.session.commit()
+    return jsonify(campo.to_json()), 201
+
+@app.route('/campo/<int:id>', methods=['PUT'])
+def update_campo(id):
+    if not request.json:
+        abort(400)
+    campo = Campo.query.get(id)
+    if campo is None:
+        abort(404)
+    campo.nome = request.json.get('nome', campo.nome)
+    campo.tipo = request.json.get('tipo', campo.tipo)
+    campo.condicao = request.json.get('condicao', campo.condicao)
+    db.session.commit()
+    return jsonify(campo.to_json())
 
 if __name__=="__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,7 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from . import app
-
-db = SQLAlchemy(app)
+from app import *
 
 
 class Campo(db.Model):
@@ -11,14 +8,23 @@ class Campo(db.Model):
     tipo = db.Column(db.Integer, nullable=False)
     condicao = db.Column(db.String(80), nullable=False)
 
+    def to_json(self):
+        return {"id" : self.id,
+                "nome" : self.nome,
+                "tipo" : self.tipo,
+                "condicao" : self.condicao}
+
 
 class Lote(db.Model):
-    pass
-
-
-class Historico(db.Model):
-    pass
-
+    __tablename__ = 'lote'
+    id = db.Column(db.Integer, primary_key=True) 
+    qtd = db.Column(db.String(80), nullable=False)
+    descricao = db.Column(db.Integer, nullable=False)
+    animais = db.relationship('Animal', backref='lote', lazy=True)
 
 class Animal(db.Model):
-    pass
+    __tablename__ = 'animal'
+    id = db.Column(db.Integer, primary_key=True) 
+    raca = db.Column(db.String(80), nullable=False)
+    peso = db.Column(db.Integer, nullable=False)
+    lote_id = db.Column(db.Integer, db.ForeignKey('lote.id'), nullable=False)
