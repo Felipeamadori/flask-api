e9571bc9455439510626cb66c506e91195be5448
Felipeamadori <49175866+Felipeamadori@users.noreply.github.com>
2022-05-29 21:20:05 -0300

    settings

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 from os import abort
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from modelEncoder import ModelEncoder
 from models import *
 from datetime import datetime
+from settings import app, db
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json_encoder = ModelEncoder
-
-db = SQLAlchemy(app)
 
 @app.route('/campos', methods=['GET'])
 def get_campos():
